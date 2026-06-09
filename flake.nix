83ef2eed77a73ab7be567b0e5890245672b66acf
Johan-Mi <johanmi@protonmail.com>
2026-06-09 12:14:01 +0200

    Make Disko follow nixpkgs
    This should be fine, I hope.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    disko.url = "github:nix-community/disko";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, disko, ... } @ inputs:
